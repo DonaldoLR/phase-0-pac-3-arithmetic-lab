b574a09aac8fc325191810b1cdc96e4b0731302f
github username <github email address>
2021-03-12 19:19:21 -0500

    created basic math functions and returned a value

---
+++ index.js
@@ -0,0 +1,39 @@
+// Function that adds two numbers
+function add(a, b){
+    return a+b;
+}
+
+// Function that subtracts two numbers
+function subtract(a, b){
+    return a-b;
+}
+
+// Function that multiplies two numbers
+function multiply(a, b){
+    return a*b;
+}
+
+// Function that divides two numbers
+function divide(a, b){
+    return a/b;
+}
+
+// Function that increments by 1
+function increment(n){
+    return n += 1;
+}
+
+// Functions that decrement by 1
+function decrement(n){
+    return n -= 1;
+}
+
+// Function that parses an interger and returns it
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+// Function that parses as a floating number and returns a decimal 
+function preserveDecimal(n){
+    return parseFloat(n);
+}
